4b30feb368b12f8e82fe6b7ebb8a495bc26199fd
Nate Pond <nathaniel.p.pond@gmail.com>
2017-03-07 07:44:04 -0500

    thrid repo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,72 +4,90 @@ window.onload = function () {
   var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
     "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
     "t", "u", "v", "w", "x", "y", "z"];
-  var monster = ["xenomorphe", "cenobite", "predator", "pyramid-head", "terminator", "freddy", "jason", "babadook", "jaws", "brundlefly", "gremlin", "pennywise", "jigsaw", "samara", "hannibal", "michael myers"];
+  var monster = ["xenomorph", "cenobite", "predator", "pyramid-head", "terminator", "freddy", "jason", "babadook", "jaws", "brundlefly", "gremlin", "pennywise", "jigsaw", "samara", "hannibal", "michael myers"];
   var word ;
   word = monster[Math.floor(Math.random() * monster.length)]
   var guess ; 
   var guesses = [ ];
-  game = function () {  
-    wordSpan = function () {
-      var wordBlank = document.getElementById("blanks");
-      var correct = document.createElement("ul");
-      for (var i = 0; i < word.length; i++) {
-        correct.setAttribute("id", "blanks");
-        guess = document.createElement("li");
-        guess.setAttribute("class", "guess");
-        if (word[i] === " ") {
-          guess.innerHTML = " ";
-        } 
-        else {
-          guess.innerHTML = "_";
-        }
-        guesses.push(guess);
-        wordBlank.appendChild(correct);
-        correct.appendChild(guess);
-      }
-    }
-    wordSpan();
-  }
-
-
-  input = function() {
-    alphabet.onkeypress = function(event) {
-      guess = event.key;
-      this.setAttribute("class", "active");
-      this.onkeyup = null;
-      for (var i = 0; i < word.length; i++) {
-        if (word[i] === geuss) {
-          geusses[i].innerHTML = geuss;
-        }
-      }
-      var i = (word.indexOf(guess));
-      if (i === -1) {
-        limbs -= 1;
-        wordSpan();
-      }
+  var wrong = [ ];
+  var right = 0;
+  word = monster[Math.floor(Math.random() * monster.length)]
+    document.getElementById("limbs").innerHTML = limbs; 
+    var wordBlank = document.getElementById("blanks");
+    var correct = document.createElement("ul");
+  game = function () { 
+    
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute("id", "blanks");
+      guess = document.createElement("li");
+      guess.setAttribute("class", "guess");
+      if (word[i] === " ") {
+        guess.innerHTML = " ";
+      } 
       else {
-        wordSpan();
+        guess.innerHTML = "_";
       }
+      guesses.push(guess);
+      wordBlank.appendChild(correct);
+      correct.appendChild(guess);
     }
+  
+
+
+    document.onkeypress = function(event) {
+     var guess = event.key;
+     guess.onkeypress = null;
+      for (var i = 0; i < word.length; i++) {
+         guess.up = null;
+        if (word[i] === guess) {
+          guesses[i].innerHTML = guess;
+          right++
+        }
+      }
+      var i = (word.indexOf(guess))
+      if (i === -1) {
+        limbs--;
+        wrong.push(guess);
+        document.getElementById("guessed").innerHTML = wrong;
+        } 
+        console.log(limbs);
+      document.getElementById("limbs").innerHTML = limbs;
+      
+      if (limbs < 1) {
+        var audio = new Audio('assets/Disappointed.mp3');
+        audio.play();
+        document.getElementById("prize").src = "assets/images/fail.png";
+        correct.parentNode.removeChild(correct);
+        limbs = 6;
+        right = 0;
+        game();
+      }
+
+      if(right === word.length) {
+        guess.onkeyup= null;
+        var audio = new Audio('assets/yay.mp3');
+        audio.play();
+        document.getElementById("prize").src = "assets/images/" + word + ".png";
+        correct.parentNode.removeChild(correct);
+        correct.parentNode.removeChild()
+        limbs = 6;
+        right = 0;
+        game();
+      }
+
+    }
+
+
   }
 
-  console.log(word);
 
-  input();
+  
+  console.log(word);
 
   game();
 
-  document.getElementById("limbs").innerHTML = limbs
 
-  document.getElementById("wins").innerHTML = wins
+
+  document.getElementById("wins").innerHTML = wins;
 
 }
-
-
-  //   document.getElementById("limbs").innerHTML = limbs
-
-  //   document.getElementById("wins").innerHTML = wins
-
-
-  // wordSpan();
-  // game(); 
